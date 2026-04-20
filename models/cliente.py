adbe39a575cbaf97b28850cd98a646342373f24a
Hugo Giffoni <hugogiffoni@gmail.com>
2026-04-20 12:19:12 +0100

    imports

---
+++ models/cliente.py
@@ -0,0 +1,8 @@
+"""
+Módulo que define a classe Cliente.
+
+Representa um cliente da loja de jogos de tabuleiro, com validações
+de email e encapsulamento dos atributos.
+"""
+import re
+from typing import Any, Optional
