d11494d3cdc64e6df139f4193cecc7ceb08b4dc6
Hugo Giffoni <hugogiffoni@gmail.com>
2026-05-11 10:25:23 +0100

    inicialaizacao app

---
+++ webservice/app.py
@@ -0,0 +1,9 @@
+"""
+Aplicação Flask - Web Service da Loja de Jogos de Tabuleiro.
+
+Este é o ponto de entrada do web service. Cria a aplicação Flask,
+regista os Blueprints (rotas) e arranca o servidor.
+
+Uso:
+    python -m webservice.app
+"""
