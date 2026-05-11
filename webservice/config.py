2ae637f06d75ab6dde48b59c18ce841fb60730c1
Hugo Giffoni <hugogiffoni@gmail.com>
2026-05-11 10:02:54 +0100

    inicializacao config

---
+++ webservice/config.py
@@ -0,0 +1,6 @@
+"""
+Configurações do Web Service Flask.
+
+Centraliza todas as definições da aplicação (porta, debug, caminho da BD)
+num só lugar, facilitando manutenção e futuras alterações.
+"""
