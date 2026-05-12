eceef426009d4b77c731c12063dc85a4d4301e4f
Hugo Giffoni <hugogiffoni@gmail.com>
2026-05-12 21:27:11 +0100

    iniciar ficheiro

---
+++ gui/paginas/clientes.py
@@ -0,0 +1,9 @@
+"""
+Página de gestão de Clientes.
+
+Funcionalidades:
+    - Listar clientes em tabela (com pesquisa)
+    - Criar novo cliente (dialog)
+    - Editar cliente existente (dialog)
+    - Apagar cliente (com confirmação)
+"""
