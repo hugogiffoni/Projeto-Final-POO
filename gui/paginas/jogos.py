3ff3fa841f6eba214ad16438d5304f02f9681938
Hugo Giffoni <hugogiffoni@gmail.com>
2026-05-12 21:57:06 +0100

    inicar ficheiro

---
+++ gui/paginas/jogos.py
@@ -0,0 +1,9 @@
+"""
+Página de gestão de Jogos.
+
+Funcionalidades:
+    - Listar jogos em tabela (com pesquisa)
+    - Criar novo jogo (dialog)
+    - Editar jogo existente (dialog)
+    - Apagar jogo (com confirmação)
+"""
