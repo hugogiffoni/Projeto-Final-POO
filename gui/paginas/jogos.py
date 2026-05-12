eb1afaa4385b56cca43edb02b9461660d96cf1eb
Hugo Giffoni <hugogiffoni@gmail.com>
2026-05-12 21:59:46 +0100

    estado

--- gui/paginas/jogos.py
+++ gui/paginas/jogos.py
@@ -11,3 +11,12 @@ from nicegui import ui
 
 from gui import api_client
 from gui.api_client import APIError
+
+# ---------------------------------------------------------------- estado
+class _Estado:
+    """Container para referências partilhadas na página."""
+    tabela: ui.table | None = None
+    campo_pesquisa: ui.input | None = None
+
+
+estado = _Estado()
