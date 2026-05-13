949d665ae51a80d6e310e3f9974dff58ff01f62d
Hugo Giffoni <hugogiffoni@gmail.com>
2026-05-13 11:10:50 +0100

    Iniciar ficheiro

---
+++ gui/paginas/historico.py
@@ -0,0 +1,9 @@
+"""
+Página de Histórico de Vendas.
+
+Funcionalidades:
+    - Listar todas as vendas (com join de cliente e jogo já no backend)
+    - Filtrar por cliente
+    - Pesquisa livre (cliente / jogo)
+    - Mostrar total faturado das vendas visíveis
+"""
